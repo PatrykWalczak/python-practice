337bd6c8ad51656e0863eee9acac5d0c7d9f8d96
pwalczak <patryk.walczak@neuca.pl>
2026-08-28 20:13:54 +0200

    feat:3xpetions exc

---
+++ 3_exceptions/number.py
@@ -0,0 +1,18 @@
+def get_int():
+    while True:
+        try:
+            x = int(input("What's x?: "))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            return x
+    
+    
+
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+
+main()
+
