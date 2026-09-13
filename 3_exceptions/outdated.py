8ac0b8936d32587bf62e3cd9ce996857fe022bd5
pwalczak <patryk.walczak@neuca.pl>
2026-09-13 12:19:39 +0200

    feat: def exe

--- 3_exceptions/outdated.py
+++ 3_exceptions/outdated.py
@@ -14,6 +14,14 @@ months_list = [
 ]
 
 
+def parse_numeric(date_str):
+    splited_list = date_str.split("/")
+    month = int(splited_list[0])
+    day = int(splited_list[1])
+    year = int(splited_list[2])
+    return month, day, year
+
+
 while True:
     ask_user = input("Date: ")
     try:
@@ -32,16 +40,22 @@ while True:
                 break
             else:
                 raise ValueError()
+            
         else:
-            # pozbycie się przecinka i podzielenie na liste 3 elementowa tego co pyta user
             splited_list = ask_user.replace(",", "").split()
+            month = months_list.index(splited_list[0]) + 1
             day = int(splited_list[1])
             year = int(splited_list[2])
-            month = months_list.index(splited_list[0]) + 1
 
             if 1 <= month <= 12 and 1 <= day <= 31:
-                
-
+                day = f"{day:02d}"
+                month = f"{month:02d}"
+                year = f"{year}-{month}-{day}"
+                print(year)
+                break
+            else:
+                raise ValueError()
+            
     except ValueError:
         print("ValueError- mozliwe problemy to: STR zamiast INT, nieprawidłowy dzień / miesiąć ")
 
